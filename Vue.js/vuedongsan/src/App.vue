298e307dfa4fba996532ee6343f698ca24f31a5a
Yeol17 <hsl0717@naver.com>
2023-01-21 17:04:09 +0900

    [Vue] Part1 15. sort

--- Vue.js/vuedongsan/src/App.vue
+++ Vue.js/vuedongsan/src/App.vue
@@ -5,22 +5,27 @@
     <Modal @closeModal="모달창열렸니=false" :원룸들="원룸들" :누른거="누른거" :모달창열렸니="모달창열렸니" />
   </div> -->
   <Transition name="fade">
-    <Modal @closeModal="모달창열렸니=false" :원룸들="원룸들" :누른거="누른거" :모달창열렸니="모달창열렸니" />
+    <Modal @closeModal="모달창열렸니 = false" :원룸들="원룸들" :누른거="누른거" :모달창열렸니="모달창열렸니" />
   </Transition>
 
   <div class="menu">
     <a href="" v-for="(메뉴, i) in 메뉴들" :key="i">{{ 메뉴 + " / " + i}}</a>
   </div>
-  
-  <DiscBanner :이름="오브젝트.name" :나이="오브젝트.age"/>
+
+  <DiscBanner :이름="오브젝트.name" :나이="오브젝트.age" />
+
+  <button @click="priceSort">가격순</button>
+  <button @click="ganadaSort">가나다순</button>
+  <button @click="underSort">50만원이하</button>
+  <button @click="sortBack">되돌리기</button>
 
   <Card @openModal="open" :원룸="원룸들[i]" v-for="(작명, i) in 원룸들" :key="i" />
- 
+
 </template>
 
 <script>
 import data from './assets/oneroom'
-import DiscBanner  from '@/Discount'
+import DiscBanner from '@/Discount'
 import Modal from '@/Modal'
 import Card from '@/Card'
 
@@ -28,9 +33,10 @@ export default {
   name: 'App',
   data() {
     return {
-      오브젝트: {name: 'kim', age: 20},
+      원룸들오리지널: [...data],
+      오브젝트: { name: 'kim', age: 20 },
       누른거: 0,
-      원룸들: data,
+      원룸들: [...data],
       모달창열렸니: false,
       메뉴들: [
         'Home', 'Shop', 'About'
@@ -41,6 +47,22 @@ export default {
     open(n) {
       this.모달창열렸니 = true;
       this.누른거 = n;
+    },
+    sortBack() {
+      this.원룸들 = [...this.원룸들오리지널]
+    },
+    priceSort() {
+      this.원룸들.sort(function (a, b) {
+        return a.price - b.price
+      })
+    },
+    ganadaSort() {
+      this.원룸들.sort(function (a, b) {
+        return a.title == b.title ? 0 : a.title > b.title ? 1 : -1;
+      })
+    },
+    underSort() {
+      this.원룸들 = this.원룸들.filter(원룸 => 원룸.price <= 500000)
     }
   },
   components: {
@@ -63,27 +85,34 @@ div {
 
 .start {
   opacity: 0;
-  transition: all .6s; 
+  transition: all .6s;
 }
+
 .end {
   opacity: 1
 }
-.fade-enter-from{
+
+.fade-enter-from {
   opacity: 0;
 }
-.fade-enter-active{
+
+.fade-enter-active {
   transition: all .4s;
 }
-.fade-enter-to{
+
+.fade-enter-to {
   opacity: 1;
 }
-.fade-leave-from{
+
+.fade-leave-from {
   opacity: 1;
 }
-.fade-leave-active{
+
+.fade-leave-active {
   transition: all .4s;
 }
-.fade-leave-to{
+
+.fade-leave-to {
   opacity: 0;
 }
 
@@ -93,6 +122,7 @@ div {
   margin: 10px;
   border-radius: 5px;
 }
+
 .menu {
   background: darkslateblue;
   padding: 15px;
@@ -108,5 +138,4 @@ div {
   width: 100%;
   margin-top: 40px;
 }
-
 </style>
