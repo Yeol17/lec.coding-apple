fe1186feb70a25b616e62fe7e0b6a0173e28eeb4
han seung yeol <hsl0717@naver.com>
2023-01-25 17:35:10 +0900

    [Vue] Part3 21. vuestargram layout

---
+++ Vue.js/vuestargram/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="header">
+    <ul class="header-button-left">
+      <li>Cancel</li>
+    </ul>
+    <ul class="header-button-right">
+      <li>Next</li>
+    </ul>
+    <img src="./assets/logo.png" class="logo" />
+  </div>
+
+  <Container :Data="Data" />
+
+  <div class="footer">
+    <ul class="footer-button-plus">
+      <input type="file" id="file" class="inputfile" />
+      <label for="file" class="input-plus">+</label>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Container from './components/Container'
+import Data from './assets/data'
+export default {
+  name: 'App',
+  data() {
+    return {
+      Data
+    }
+  },
+  components: {
+    Container,
+  }
+}
+</script>
+
+<style>
+@import './style.css';
+</style>
---
+++ Vue.js/vuestargram/src/components/Container.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Post :Data="Data[i]" v-for="(item, i) in Data" :key="i" />
+  </div>
+</template>
+<script>
+import Post from './Post'
+
+export default {
+  name: 'Container',
+  components: {
+    Post,
+  },
+  props: {
+    Data: Array
+  }
+}
+</script>
+<style>
+
+</style>
