2609b1c7502fa2f63680cc8859993254af394249
Yeol17 <hsl0717@naver.com>
2023-01-26 23:43:35 +0900

    [Vue] Part3 25. file upload

--- Vue.js/vuestargram/src/App.vue
+++ Vue.js/vuestargram/src/App.vue
@@ -9,18 +9,21 @@
     <img src="./assets/logo.png" class="logo" />
   </div>
 
-  <Container :data="data" :step="step"/>
+  <Container :data="data" :step="step" :url="url" />
 
   <button @click="more">더보기</button>
 
   <div class="footer">
     <ul class="footer-button-plus">
-      <input type="file" id="file" class="inputfile" />
+      <input
+        @change="upload"
+        type="file"
+        id="file"
+        class="inputfile"
+      />
       <label for="file" class="input-plus">+</label>
     </ul>
   </div>
-
-
 
   <!-- <div v-if="tab == 0">내용0</div>
   <div v-if="tab == 1">내용1</div>
@@ -28,41 +31,47 @@
   <button @click="tabView(0)">버튼0</button>
   <button @click="tabView(1)">버튼1</button>
   <button @click="tabView(2)">버튼2</button> -->
-
 </template>
 
 <script>
-import Container from './components/Container'
-import Data from './assets/data'
-import axios from 'axios'
+import Container from "./components/Container";
+import Data from "./assets/data";
+import axios from "axios";
 
 export default {
-  name: 'App',
+  name: "App",
   data() {
     return {
       data: Data,
       cnt: 0,
-      step: 2,
-    }
+      step: 0,
+      url: '',
+    };
   },
   components: {
     Container,
   },
   methods: {
     more() {
-      axios.get(`https://codingapple1.github.io/vue/more${this.cnt}.json`)
-        .then(결과 => {
+      axios
+        .get(`https://codingapple1.github.io/vue/more${this.cnt}.json`)
+        .then((결과) => {
           this.data.push(결과.data);
-          this.cnt++
-        })
+          this.cnt++;
+        });
     },
     tabView(i) {
-      this.tab = i 
-    }
-  }
-}
+      this.tab = i;
+    },
+    upload(e) {
+      let file = e.target.files;
+      this.url = URL.createObjectURL(file[0]);
+      this.step = 1;
+    },
+  },
+};
 </script>
 
 <style>
-@import './style.css';
+@import "./style.css";
 </style>
--- Vue.js/vuestargram/src/components/Container.vue
+++ Vue.js/vuestargram/src/components/Container.vue
@@ -6,7 +6,7 @@
 
     <!-- 필터선택페이지 -->
     <div v-if="step == 1">
-      <div class="upload-image"></div>
+      <div class="upload-image" :style="{backgroundImage: `url(${url})`}"></div>
       <div class="filters">
         <div class="filter-1"></div>
         <div class="filter-1"></div>
@@ -18,7 +18,7 @@
 
     <!-- 글작성페이지 -->
     <div v-if="step == 2">
-      <div class="upload-image"></div>
+      <div class="upload-image" :style="{backgroundImage: `url(${url})`}"></div>
       <div class="write">
         <textarea class="write-box">write!</textarea>
       </div>
@@ -36,7 +36,8 @@ export default {
   },
   props: {
     data: Array,
-    step: Number
+    step: Number,
+    url: String
   }
 }
 </script>
