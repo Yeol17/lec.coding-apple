19382dbd447b542b456b3d19e4a18b16db25d7f1
Yeol17 <hsl0717@naver.com>
2023-01-29 15:36:23 +0900

    [Vue] Part3 27.cssgram

--- Vue.js/vuestargram/src/components/Container.vue
+++ Vue.js/vuestargram/src/components/Container.vue
@@ -7,42 +7,86 @@
 
     <!-- 필터선택페이지 -->
     <div v-if="step == 1">
-      <div class="upload-image" :style="{ backgroundImage: `url(${img})` }"></div>
+      <div
+        class="upload-image"
+        :style="{ backgroundImage: `url(${img})` }"
+      ></div>
       <div class="filters">
-        <div class="filter-1"></div>
-        <div class="filter-1"></div>
-        <div class="filter-1"></div>
-        <div class="filter-1"></div>
-        <div class="filter-1"></div>
+        <FilterBox
+          :img="img"
+          :class="filter"
+          v-for="(filter, i) in filters"
+          :key="i"
+        ></FilterBox>
       </div>
     </div>
 
     <!-- 글작성페이지 -->
     <div v-if="step == 2">
-      <div class="upload-image" :style="{ backgroundImage: `url(${img})` }"></div>
+      <div
+        class="upload-image"
+        :style="{ backgroundImage: `url(${img})` }"
+      ></div>
       <div class="write">
-        <textarea @input="$emit('write', $event.target.value)" class="write-box">write!</textarea>
+        <textarea
+          @input="$emit('write', $event.target.value)"
+          class="write-box"
+        >
+write!
+        </textarea>
       </div>
     </div>
-
   </div>
 </template>
 <script>
-import Post from './Post'
+import Post from "./Post";
+import FilterBox from "./FilterBox.vue";
 
 export default {
-  name: 'Container',
+  name: "Container",
+  data() {
+    return {
+      filters: [
+        "aden",
+        "_1977",
+        "brannan",
+        "brooklyn",
+        "clarendon",
+        "earlybird",
+        "gingham",
+        "hudson",
+        "inkwell",
+        "kelvin",
+        "lark",
+        "lofi",
+        "maven",
+        "mayfair",
+        "moon",
+        "nashville",
+        "perpetua",
+        "reyes",
+        "rise",
+        "slumber",
+        "stinson",
+        "toaster",
+        "valencia",
+        "walden",
+        "willow",
+        "xpro2",
+      ],
+    };
+  },
   components: {
     Post,
+    FilterBox,
   },
   props: {
     data: Array,
     step: Number,
     img: String,
-    more: Function
+    more: Function,
   },
-
-}
+};
 </script>
 <style>
 .upload-image {
