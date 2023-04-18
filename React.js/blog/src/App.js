51866c615650dcf604e68ce21f5b7b6095ae6554
Yeol17 <hsl0717@naver.com>
2023-04-18 14:05:47 +0900

    [React] Part1 11.input1

--- React.js/blog/src/App.js
+++ React.js/blog/src/App.js
@@ -14,6 +14,7 @@ function App() {
     return data
   });
   let [modal, setModal] = useState(false);
+  let [입력값, 입력값변경] = useState('');
 
   return (
     <div className="App">
@@ -21,11 +22,11 @@ function App() {
         <h4>ReactBlog</h4>
       </div>
 
-      <button onClick={() => {
+      {/* <button onClick={() => {
         let copy = [...글제목];
         copy.sort();
         제목변경(copy);
-      }}>가나다</button>
+      }}>가나다</button> */}
 
       {/* <div className="list" onClick={() => {
         setModal(!modal);
@@ -37,16 +38,29 @@ function App() {
       {
         글제목.map(function (a, i) {
           return (
-            <div className="list" key={i}>
-              <h4 onClick={() => {
-                setModal(!modal)
-                setIdx(i)
-              }}>{a} <span onClick={() => {
-                let copy = [...like];
-                copy[i]++;
-                likeChange(copy)
-              }}>❤️</span>{like[i]}</h4>
-              <p>2월 17일 발행</p>
+            <div style={{height: '120px'}}className="list" key={i}>
+              <div style={{float: 'left', width: '90%'}}>
+                <h4 onClick={() => {
+                  setModal(!modal)
+                  setIdx(i)
+                }}>{a} <span onClick={(e) => {
+                  e.stopPropagation();
+                  let copy = [...like];
+                  copy[i]++;
+                  likeChange(copy)
+                }}>❤️</span>{like[i]}</h4>
+                <p>2월 17일 발행</p>
+              </div>
+              <button style={{float: 'right', margin: '6px 6px 0 0'}} onClick={() => {
+                let copy = [...글제목];
+                copy.splice(i, 1);
+                제목변경(copy);
+                let likeCopy = [...like];
+                // console.log(likeCopy);
+                likeCopy.splice(i, 1);
+                likeChange(likeCopy);
+                // console.log(like);
+              }}>X</button>
             </div>
           )
         })
@@ -58,11 +72,32 @@ function App() {
         console.log(글제목, 글);
         제목변경(글);
       }}>이상한버튼</button> */}
+
+
+      <div style={{ marginTop: 10 }}>
+        <input onChange={(e) => {
+          입력값변경(e.target.value);
+        }}></input>
+        <button onClick={() => {
+          console.log(like);
+          if(입력값 == '') return
+          let copy = [...글제목];
+          copy.push(입력값);
+          제목변경(copy);
+          let likeCopy = [...like]
+          likeCopy.push(0);
+          likeChange(likeCopy);
+        }}>글쓰기</button>
+      </div>
+
+
       {
         modal ? <Modal 글제목={글제목} title={title} /> : null
       }
 
-    </div>
+
+
+    </div >
 
   );
 }
