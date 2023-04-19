041ab11d1c8132c81bf9221f04e6beaddb71af84
Yeol17 <hsl0717@naver.com>
2023-04-19 20:00:48 +0900

    [React] Part2 3.import-export

--- React.js/shop/src/App.js
+++ React.js/shop/src/App.js
@@ -1,8 +1,13 @@
+import { useState } from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import './App.css';
-import bg from './images/bg.png'
+import bg from './images/bg.png';
+import data from './data';
 
 function App() {
+
+  let [shoes] = useState(data);
+
   return (
     <div className="App">
       <Navbar bg="dark" variant="dark">
@@ -28,25 +33,24 @@ function App() {
 
       <div className="container">
         <h3>상품목록</h3>
-        <div className="row prods">
-          <div className="col-md-4 item">
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%"alt="" />
-            <h4>껌정이</h4>
-            <p>상품설명</p>
-          </div>
-          <div className="col-md-4 item">
-            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%"alt="" />
-            <h4>분홍이</h4>
-            <p>상품설명</p>
-          </div>
-          <div className="col-md-4 item">
-            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%"alt="" />
-            <h4>그레이</h4>
-            <p>상품설명</p>
-          </div>
+        <div className="row prods mx-auto">
+          {shoes.map((shoe) => {
+            return (
+              <Card shoe={shoe} key={shoe.id} />
+            )
+          })}
         </div>
       </div>
+    </div>
+  )
+}
 
+function Card(props) {
+  return (
+    <div className="col-md-4 item">
+      <img src={props.shoe.src} width="80%" alt={`신발이미지${props.shoe.id}`} />
+      <h4>{props.shoe.title}</h4>
+      <p>{props.shoe.content}</p>
     </div>
   )
 }
