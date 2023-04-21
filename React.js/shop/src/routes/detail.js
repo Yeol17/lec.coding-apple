bc5772672f73ad7c8d5e828774546d77542c782f
Yeol17 <hsl0717@naver.com>
2023-04-21 15:35:31 +0900

    [React] Part2.7.route3

--- React.js/shop/src/routes/detail.js
+++ React.js/shop/src/routes/detail.js
@@ -1,18 +1,35 @@
-function Detail() {
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-6">
-          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-        </div>
-        <div className="col-md-6">
-          <h4 className="pt-5">상품명</h4>
-          <p>상품설명</p>
-          <p>120000원</p>
-          <button className="btn btn-danger">주문하기</button>
+import { useParams } from "react-router-dom";
+
+function Detail(props) {
+
+  let { id } = useParams();
+  let shoes = [...props.shoes];
+  let res = shoes.find(shoe => shoe.id == id)
+
+  if (res) {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <img src={res.src} width="100%" />
+          </div>
+          <div className="col-md-6">
+            <h4 className="pt-5">{res.title}</h4>
+            <p>{res.content}</p>
+            <p>{res.price}원</p>
+            <button className="btn btn-danger">주문하기</button>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  } else {
+    return (
+      <div className="container">
+        <div className="row">
+          <p>올바르지 않은 페이지입니다.</p>
+        </div>
+      </div>
+      )
+  }
 }
 export default Detail;
